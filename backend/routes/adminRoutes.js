559290a9015c9bdb4a380cad3e4c870fba6fbd80
vijju_reddy <vijayalakshmivangala476@gmail.com>
2026-06-21 17:01:12 +0530

    Delete backend/routes directory

--- backend/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getMapDashboard } from "../controllers/adminController.js";
-import { authorizeRoles, protect } from "../middleware/authMiddleware.js";
-
-const adminRouter = express.Router();
-
-adminRouter.get("/map-dashboard", protect, authorizeRoles("admin"), getMapDashboard);
-
-export default adminRouter;
